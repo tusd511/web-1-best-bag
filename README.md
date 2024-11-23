4559bf90b92ae69dc03212c2cde1486405951bc9
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2024-11-23 12:08:29 +0700

    them data hoa-don, fix data nguoi-dung

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Web 1 Best Bag
 
 - do an web 1 k24
+
+### test data summary
+
+- thoi gian luu iso string timezone +07:00
+- 750 san pham
+- 100 nguoi dung
+  - 2 chu cuoi ten viet lien khong dau dung cho
+    - sau dau cham email
+    - truoc dau tru password
+  - ngay tao trong nam 2024, truoc 23/11/2024
+  - bi khoa tai khoan: ty le 10%
+- 200 hoa don
+  - chi chon trong 100 san pham
+  - chi chon trong 50 nguoi dung
+  - ngay tao trong nam 2024, truoc 23/11/2024
+  - toi da 5 chi tiet moi hoa don
+    - toi da 5 so luong san pham moi chi tiet
+  - da xac nhan: ty le 98%
--- tao-du-lieu.py
+++ tao-du-lieu.py
@@ -1,22 +1,31 @@
-from re import L
-from typing import Callable
-from faker import Faker as F
-from csv import DictWriter as D
-from unidecode import unidecode as u
 from argparse import ArgumentParser as P
-from json import dump as j
+from csv import DictWriter as D
+import json as j
+import random as r
+from typing import Callable
+import datetime as d
+from faker import Faker as F
+from unidecode import unidecode as u
+
+f = F(locale="vi_VN", use_weighting=False)
+tz = d.timezone(d.timedelta(hours=7))
 
 
 def save_to_csv(data: list[dict], filename: str):
-    with open(filename, "w", encoding="utf-8", newline="") as f:
-        w = D(f, fieldnames=data[0].keys())
+    with open(filename, "w", encoding="utf-8", newline="") as o:
+        w = D(o, fieldnames=data[0].keys())
         w.writeheader()
         w.writerows(data)
 
 
 def save_to_json(data: list[dict], filename: str):
-    with open(filename, "w", encoding="utf-8", newline="") as f:
-        j(data, f)
+    with open(filename, "w", encoding="utf-8", newline="") as o:
+        j.dump(data, o)
+
+
+def read_from_json(filename: str):
+    with open(filename, "r", encoding="utf-8") as f:
+        return j.load(f)
 
 
 def create_fake(dataname: str, gen_func: Callable[[int], list[dict]], count: int):
@@ -26,11 +35,10 @@ def create_fake(dataname: str, gen_func: Callable[[int], list[dict]], count: int
 
 
 def generate_fake_users(n) -> list[dict]:
-    f = F(locale="vi_VN", use_weighting=False)
     return [
         {
             "id": f.uuid4(),
-            "full_name": (name := f.name()),
+            "name": (name := f.name()),
             "email": f.free_email().replace(
                 "@", f".{(sname := u("".join(name.split()[-2:])).lower())}@"
             ),
@@ -41,6 +49,7 @@ def generate_fake_users(n) -> list[dict]:
                 upper_case=True,
                 lower_case=True,
             )}",
+            "ngay-tao": f.date_time_this_year(before_now=True, tzinfo=tz).isoformat(),
             "disabled": f.boolean(chance_of_getting_true=10),
         }
         for _ in range(n)
@@ -48,7 +57,32 @@ def generate_fake_users(n) -> list[dict]:
 
 
 def generate_fake_receipt(n) -> list[dict]:
-    pass
+    n_sp_use = 100
+    n_nd_use = 50
+    sp_per_receipt = 5
+    sp_n_per_detail = 5
+    sps: list[str] = list(
+        map(
+            lambda sp: sp["web-scraper-order"],
+            r.sample(read_from_json("san-pham.json"), n_sp_use),
+        )
+    )
+    nds: list[str] = list(
+        map(lambda nd: nd["id"], r.sample(read_from_json("nguoi-dung.json"), n_nd_use))
+    )
+    return [
+        {
+            "id": f.uuid4(),
+            "nguoi-dung": r.choice(nds),
+            "ngay-tao": f.date_time_this_year(before_now=True, tzinfo=tz).isoformat(),
+            "chi-tiet": [
+                {"san-pham": sp, "so-luong": r.randint(1, sp_n_per_detail)}
+                for sp in r.sample(sps, k=r.randint(1, sp_per_receipt))
+            ],
+            "da-xac-nhan": f.boolean(chance_of_getting_true=98),
+        }
+        for _ in range(n)
+    ]
 
 
 data_types = {"nguoi-dung": generate_fake_users, "hoa-don": generate_fake_receipt}
